7ecaa9aaf41ee9fa12157e630c9a8f5df8c5f73d
sajid73 <smsajid7300@gmail.com>
2026-03-22 23:49:15 +0600

    chore: mobile application setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
-import { initDB } from "./src/config/db.js";
-import rateLimiter from "./src/middleware/rateLimiter.js";
+import { initDB } from "./config/db.js";
+import rateLimiter from "./middleware/rateLimiter.js";
 
-import transactionsRoute from "./src/routes/transactionsRoute.js"
+import transactionsRoute from "./routes/transactionsRoute.js"
 
 dotenv.config()
 
---
+++ mobile/app/about.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const About = () => {
+  return (
+    <View>
+      <Text>About none s</Text>
+    </View>
+  )
+}
+
+export default About
